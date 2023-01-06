47a94b427da259b08117e7a4b558c5e4998bc54a
sakamuuy <sakamuu.y@gmail.com>
2023-01-06 12:07:36 +0900

    fix generate_id

--- src/todo.rs
+++ src/todo.rs
@@ -6,7 +6,7 @@ pub struct Todo {
     pub due: String,
     pub title: String,
     pub description: String,
-    pub is_completed: bool
+    pub is_completed: bool,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -20,25 +20,33 @@ pub fn show_all_todo_list(todo_list: &TodoList) {
     }
 }
 
+fn generate_id(current_todo_list: &TodoList) -> usize {
+    let todos = current_todo_list.todos.clone();
+    let mut current_id: usize = 0;
+    for todo in todos {
+        if todo.id > current_id {
+            current_id = todo.id;
+        }
+    }
+
+    return current_id + 1;
+}
+
 pub fn add_new_todo(
     due: &str,
     title: &str,
     description: &str,
     current_todo_list: &TodoList,
 ) -> TodoList {
-    let mut id: usize = 1;
-    for todo in current_todo_list.todos.into_iter() {
-        if todo.id == id {
-            id = todo.id + 1;
-        }
-    }
+    let id = generate_id(&current_todo_list);
     let todo: Todo = Todo {
         id: id,
         due: String::from(due),
         title: String::from(title),
         description: String::from(description),
-        is_completed: false
+        is_completed: false,
     };
+
     let mut new_todo_list = current_todo_list.todos.clone();
     new_todo_list.push(todo);
     return TodoList {
